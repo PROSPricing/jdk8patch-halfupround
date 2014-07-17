f848353f03f38bf877dff0bd3c9a040042b2ae51
William Price <wprice@pros.com>
2014-07-17 13:30:40 -0500

    Set up Gradle project for publishing

--- src/main/java/com/pros/java/text/DigitList.java
+++ src/main/java/com/pros/java/text/DigitList.java
@@ -4,12 +4,22 @@
  *
  * This code is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License version 2 only, as
- * published by the Free Software Foundation.  This code is a DERIVATIVE
- * WORK of source code previously released and designated, by Oracle, as
- * subject to the "Classpath" exception as provided by Oracle in the
- * LICENSE file of the original work; the same LICENSE file accompanies
- * this code.  As a DERIVATIVE WORK, the authors make this code subject
- * to the same license terms and conditions.
+ * published by the Free Software Foundation.
+ *
+ * Linking this library statically or dynamically with other modules is making
+ * a combined work based on this library.  Thus, the terms and conditions of
+ * the GNU General Public License cover the whole combination.
+ *
+ * As a special exception, the copyright holders of this library give you
+ * permission to link this library with independent modules to produce an
+ * executable, regardless of the license terms of these independent modules,
+ * and to copy and distribute the resulting executable under terms of your
+ * choice, provided that you also meet, for each linked independent module,
+ * the terms and conditions of the license of that module.  An independent
+ * module is a module which is not derived from or based on this library.  If
+ * you modify this library, you may extend this exception to your version of
+ * the library, but you are not obligated to do so.  If you do not wish to do
+ * so, delete this exception statement from your version.
  *
  * This code is distributed in the hope that it will be useful, but WITHOUT
  * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
@@ -42,8 +52,8 @@ abstract class DigitList
      * Shim method to be inserted into {@code java.text.DigitList} by the patch routine.
      * <p>
      * User <q>Holger</q> identified the cause of the bug,
-     * <a href="http://stackoverflow.com/questions/24426438/numberformat-rounding-issue-with-java-8-only/24426907#24426907">
-     * posted on StackOverflow.com</a>, as the following switch case inside
+     * <a href="http://stackoverflow.com/a/24427356/2390644">posted on StackOverflow.com</a>,
+     * as the following switch case inside
      * {@code boolean shouldRoundUp(int, boolean, boolean)} (line 522).
      * <pre>
      * case HALF_UP:
